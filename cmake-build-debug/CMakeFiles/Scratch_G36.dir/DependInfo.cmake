42426fdfad4811350f0f5bd833024a7e788196be
mahan <mahan241185@gmail.com>
2026-02-22 15:31:27 +0330

    spirit texture added

--- cmake-build-debug/CMakeFiles/Scratch_G36.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Scratch_G36.dir/DependInfo.cmake
@@ -8,19 +8,23 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "D:/SUT/term 1/programing/PROJECT/Scratch (1)/Scratch/scripts/CodePanel/codePanel.cpp" "CMakeFiles/Scratch_G36.dir/scripts/CodePanel/codePanel.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/CodePanel/codePanel.cpp.obj.d"
-  "D:/SUT/term 1/programing/PROJECT/Scratch (1)/Scratch/scripts/Common/common.cpp" "CMakeFiles/Scratch_G36.dir/scripts/Common/common.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/Common/common.cpp.obj.d"
-  "D:/SUT/term 1/programing/PROJECT/Scratch (1)/Scratch/scripts/CostumesPanel/CostumesPanel.cpp" "CMakeFiles/Scratch_G36.dir/scripts/CostumesPanel/CostumesPanel.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/CostumesPanel/CostumesPanel.cpp.obj.d"
-  "D:/SUT/term 1/programing/PROJECT/Scratch (1)/Scratch/scripts/LeftPannel/ModeSelector/ModeSelector.cpp" "CMakeFiles/Scratch_G36.dir/scripts/LeftPannel/ModeSelector/ModeSelector.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/LeftPannel/ModeSelector/ModeSelector.cpp.obj.d"
-  "D:/SUT/term 1/programing/PROJECT/Scratch (1)/Scratch/scripts/Menu/menu.cpp" "CMakeFiles/Scratch_G36.dir/scripts/Menu/menu.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/Menu/menu.cpp.obj.d"
-  "D:/SUT/term 1/programing/PROJECT/Scratch (1)/Scratch/scripts/SpiritFunctions/spiritFunctions.cpp" "CMakeFiles/Scratch_G36.dir/scripts/SpiritFunctions/spiritFunctions.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/SpiritFunctions/spiritFunctions.cpp.obj.d"
-  "D:/SUT/term 1/programing/PROJECT/Scratch (1)/Scratch/scripts/SpiritPreview/spiritPreview.cpp" "CMakeFiles/Scratch_G36.dir/scripts/SpiritPreview/spiritPreview.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/SpiritPreview/spiritPreview.cpp.obj.d"
-  "D:/SUT/term 1/programing/PROJECT/Scratch (1)/Scratch/scripts/Ui/uiFuncs.cpp" "CMakeFiles/Scratch_G36.dir/scripts/Ui/uiFuncs.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/Ui/uiFuncs.cpp.obj.d"
-  "D:/SUT/term 1/programing/PROJECT/Scratch (1)/Scratch/scripts/main.cpp" "CMakeFiles/Scratch_G36.dir/scripts/main.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/main.cpp.obj.d"
+  "E:/programming/scratch-app-winter-1404/scripts/CodePanel/CodePanel.cpp" "CMakeFiles/Scratch_G36.dir/scripts/CodePanel/CodePanel.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/CodePanel/CodePanel.cpp.obj.d"
+  "E:/programming/scratch-app-winter-1404/scripts/CostumesPanel/CostumesPanel.cpp" "CMakeFiles/Scratch_G36.dir/scripts/CostumesPanel/CostumesPanel.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/CostumesPanel/CostumesPanel.cpp.obj.d"
+  "E:/programming/scratch-app-winter-1404/scripts/LeftPannel/ModeSelector/ModeSelector.cpp" "CMakeFiles/Scratch_G36.dir/scripts/LeftPannel/ModeSelector/ModeSelector.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/LeftPannel/ModeSelector/ModeSelector.cpp.obj.d"
+  "E:/programming/scratch-app-winter-1404/scripts/Menu/menu.cpp" "CMakeFiles/Scratch_G36.dir/scripts/Menu/menu.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/Menu/menu.cpp.obj.d"
+  "E:/programming/scratch-app-winter-1404/scripts/SpiritFunctions/spiritFunctions.cpp" "CMakeFiles/Scratch_G36.dir/scripts/SpiritFunctions/spiritFunctions.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/SpiritFunctions/spiritFunctions.cpp.obj.d"
+  "E:/programming/scratch-app-winter-1404/scripts/SpiritPreview/spiritPreview.cpp" "CMakeFiles/Scratch_G36.dir/scripts/SpiritPreview/spiritPreview.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/SpiritPreview/spiritPreview.cpp.obj.d"
+  "E:/programming/scratch-app-winter-1404/scripts/Ui/uiFuncs.cpp" "CMakeFiles/Scratch_G36.dir/scripts/Ui/uiFuncs.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/Ui/uiFuncs.cpp.obj.d"
+  "E:/programming/scratch-app-winter-1404/scripts/common/common.cpp" "CMakeFiles/Scratch_G36.dir/scripts/common/common.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/common/common.cpp.obj.d"
+  "E:/programming/scratch-app-winter-1404/scripts/main.cpp" "CMakeFiles/Scratch_G36.dir/scripts/main.cpp.obj" "gcc" "CMakeFiles/Scratch_G36.dir/scripts/main.cpp.obj.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- scripts/main.cpp
+++ scripts/main.cpp
@@ -10,6 +10,7 @@
 
 using namespace std;
 
+
 vector<Button> modesList = {
     {
         {0, 120, 150, 50},
@@ -42,12 +43,9 @@ vector<Button> modesList = {
 
 
 int main(int argc, char *args[]) {
-    cout << "HELLO WORLD!" << endl;
-    srand(time(nullptr));
-
     // <<--------------------------------------------- initializing phase --------------------------------------------->>
 
-    if (!prepPhase(24)) {
+    if (!prepPhase(24, 14)) {
         cout << "initialization failed" << endl;
         return 1; // initialization failed
     }
@@ -55,29 +53,66 @@ int main(int argc, char *args[]) {
     // <<--------------------------------------------- program loop --------------------------------------------->>
 
     // testing part
-    setSpiritX(1500);
+    changeSpiritX(1920);
 
     bool isMouseDown = false;
+    int executeIndex = 0;
     while (scratchApp.isRunning) {
         SDL_Event event;
+        // normal executing mode ---------------->
+        if (!scratchApp.isInDebug && scratchApp.isExecuting) {
+            if (executeIndex < scratchApp.executeList.size()) {
+                cout << "mode : " << scratchApp.executeList[executeIndex].blockMode << "-- name : " << scratchApp.
+                        executeList[executeIndex].blockName << endl;
+                executeIndex++;
+            } else {
+                cout << "<-------- no execution -------->" << endl;
+                executeIndex = 0;
+                scratchApp.isExecuting = false;
+            }
+        }
+
         while (SDL_PollEvent(&event)) {
+            // closing app
             if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE) {
                 scratchApp.isRunning = false;
                 break;
             }
 
             // Handles clicking
-            if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
-                isMouseDown = true;
-
-            if (event.type == SDL_MOUSEBUTTONUP && event.button.button == SDL_BUTTON_LEFT)
-                isMouseDown = false;
-
             isClicked(event);
 
-            // Handles scrolling
-            if (event.type == SDL_MOUSEWHEEL) {
-                updateScrolloffset(840, 950, event);
+
+            if (scratchApp.selectedMode == "Code") {
+                if (event.type == SDL_MOUSEWHEEL) {
+                    // Handles scrolling for left side menu
+                    if (isPointerInElement({110, 190, 300, 840}, scratchApp.mouse.x, scratchApp.mouse.y))
+                        updateScrolloffsetCodeMenu(840, 1500, event);
+                    // Handle scrolling for playground
+                    if (isPointerInElement({400, 190, 900, 840}, scratchApp.mouse.x, scratchApp.mouse.y))
+                        updateScrolloffsetBlockPlayGround(900, 1500, event);
+                }
+
+                // removes last added block to the execute list
+                if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_BACKSPACE && event.key.repeat == 0 &&
+                    scratchApp.executeList.size() > 0) {
+                    removeLastBlockFromExecuteList();
+                }
+            }
+
+            // debug executing mode ---------------->
+            if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_SPACE && event.key.repeat == 0) {
+                if (scratchApp.isExecuting && scratchApp.isInDebug) {
+                    if (executeIndex < scratchApp.executeList.size()) {
+                        cout << "mode : " << scratchApp.executeList[executeIndex].blockMode << "-- name : " <<
+                                scratchApp.executeList[executeIndex].blockName << endl;
+                        executeIndex++;
+                    } else {
+                        executeIndex = 0;
+                        scratchApp.isExecuting = false;
+                        cout << "<-------- no execution -------->" << endl;
+                    }
+                }
             }
 
             updateMouse(isMouseDown);
@@ -99,10 +134,11 @@ int main(int argc, char *args[]) {
         if (scratchApp.selectedMode == "Code") {
             renderCodePanel();
         } else if (scratchApp.selectedMode == "Costumes") {
-            renderScrollableMenu(20 , 190 , 1300 , 1030);
+            // renderScrollableMenu(20 , 190 , 1300 , 1030);
         }
 
         // Spirit preview and stats
+        renderRunAndDebugButtons();
         renderGamePreview();
         renderSpiritStats();
 
